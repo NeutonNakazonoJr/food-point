21ab59ff54dd26a0eac87034c3145d644e2e1541
delfo <carlos13bem@gmail.com>
2024-04-12 11:11:49 -0300

    bug/css login page bug fixed in tiny screens

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
-const express = require('express');
-const dotenv = require('dotenv');
+const path = require("path");
+const express = require("express");
+const dotenv = require("dotenv");
 const app = express();
-const router = require('./routes/router');
+const router = require("./routes/router");
 
 dotenv.config();
+
+app.use("/", express.static(path.join(__dirname, "../public")));
 
 app.use(express.json());
 app.use(router);
 
-app.listen(process.env.PORT);
+app.listen(process.env.PORT || 3000);
