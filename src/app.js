a60c03a00d09f2efb5054d08bdbed5ff35c51217
delfo <carlos13bem@gmail.com>
2024-04-16 19:59:46 -0300

    upd: cors remove & cookie in default value

--- src/app.js
+++ src/app.js
@@ -6,11 +6,7 @@ const router = require("./routes/router");
 const app = express();
 
 app.use(express.json());
-app.use(
-	cors({
-		origin: "*",
-	})
-);
+// TO DO: CORS
 app.use(cookieParser());
 app.use(router);
 app.listen(PORT, () => console.log("server running!"));
