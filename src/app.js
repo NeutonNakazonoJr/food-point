78ad2d38f1cd0e59b323824255a42bb9fd54534b
delfo <carlos13bem@gmail.com>
2024-04-16 14:02:41 -0300

    update: config file to handle with dotenv configs

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const dotenv = require("dotenv");
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 const app = express();
 const router = require("./routes/router");
+const { PORT } = require("./config/config");
 
 dotenv.config();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(router);
-app.listen(process.env.PORT || 3000);
+app.listen(PORT, () => console.log("server running!"));
--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,24 +1,25 @@
-const { getUserByEmail } = require('../repositories/userRepository.js');
-const { createToken } = require('../utils/jwt.js');
+const { getUserByEmail } = require("../repositories/userRepository.js");
+const { createToken } = require("../utils/jwt.js");
+const { SECRET_KEY_JWT } = require("../config/config.js");
 
 const loginAuthenticationController = async (req, res) => {
-    try {
- 
-        const [ userInfos ] = await getUserByEmail(req.body.email);
-        const token = createToken({ userId: userInfos.id } , process.env.SECRET_KEY_JWT, { expiresIn: '8h' });
-        
-        const cookieOptions = {
-            maxAge: 8 * 60 * 60 * 1000, 
-            httpOnly: true,
-            sameSite: 'strict'
-        };
+	try {
+		const [userInfos] = await getUserByEmail(req.body.email);
+		const token = createToken({ userId: userInfos.id }, SECRET_KEY_JWT, {
+			expiresIn: "8h",
+		});
 
-        res.cookie('session_token', token, cookieOptions);
-        return res.status(200).json({ success: true });
+		const cookieOptions = {
+			maxAge: 8 * 60 * 60 * 1000,
+			httpOnly: true,
+			sameSite: "strict",
+		};
 
-    } catch (error) {
-        return res.status(500).json({ error: 'Erro interno no servidor'});
-    }
-}
+		res.cookie("session_token", token, cookieOptions);
+		return res.status(200).json({ success: true });
+	} catch (error) {
+		return res.status(500).json({ error: "Erro interno no servidor" });
+	}
+};
 
 module.exports = loginAuthenticationController;
