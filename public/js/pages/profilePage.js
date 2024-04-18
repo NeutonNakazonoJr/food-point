d0ce31587bba4b7958d87aed395c77417ec21ad1
Lígia Santiago <santiago.ligia.751@gmail.com>
2024-04-17 22:37:34 -0300

    css: position of the name and image from the user

--- public/js/pages/profilePage.js
+++ public/js/pages/profilePage.js
@@ -9,16 +9,55 @@ const createProfile = () => {
     body.id = "profile-body"
     page.appendChild(body)
 
-    const leftBar = document.createElement("div");
-    leftBar.id = "left-bar"
-    body.appendChild(leftBar);
+    const leftSide = document.createElement("div");
+    leftSide.id = "left-side"
+    body.appendChild(leftSide);
     const buttonArea = document.createElement("div");
     buttonArea.id = "button-area";
     const back = document.createElement("button");
     back.id = "back";
     back.textContent = "Voltar";
-    leftBar.appendChild(buttonArea)
+    const backIcon = document.createElement("img");
+    backIcon.className = "profile-icon"
+    backIcon.src = "./assets/icons/flag-back.svg";
+    backIcon.alt = "icone de flecha para voltar"
+    leftSide.appendChild(buttonArea);
     buttonArea.appendChild(back);
+    back.appendChild(backIcon);
+
+    const rightSide = document.createElement("div");
+    rightSide.id = "right-side";
+    body.appendChild(rightSide);
+
+    const background = document.createElement("div");
+    background.id = "background";
+    rightSide.appendChild(background)
+
+    //profile image area
+    const headerProfile = document.createElement("div");
+    headerProfile.id = "header-profile";
+    rightSide.appendChild(headerProfile);
+    const profileImage = document.createElement("div");
+    profileImage.id = "profile-image";
+    headerProfile.appendChild(profileImage);
+
+    //username and image edit anchor
+    const textarea = document.createElement("div");
+    textarea.id = "name-area";
+    const name = document.createElement("h2");
+    name.textContent = "Name Complete";
+    const anchor = document.createElement("a");
+    anchor.id = "anchor"
+    anchor.href = ""
+    anchor.textContent = "Editar foto de perfil"
+    const aIcon = document.createElement("img");
+    aIcon.className = "profile-icon"
+    aIcon.src = "./assets/icons/pencil.svg";
+    aIcon.alt = "Icone de um lápis;"
+    textarea.appendChild(name);
+    textarea.appendChild(anchor);
+    anchor.appendChild(aIcon)
+    headerProfile.appendChild(textarea)
     return page;
 }
 export default createProfile
