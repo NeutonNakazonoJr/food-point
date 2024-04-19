c8d6b7c7daa604a9ed641cc89539c6b8ca4541a7
Lígia Santiago <santiago.ligia.751@gmail.com>
2024-04-19 19:33:00 -0300

    improvements on css and javascript, implementation of local image upload.

--- public/js/pages/profilePage.js
+++ public/js/pages/profilePage.js
@@ -5,10 +5,12 @@ const createProfile = () => {
     page.id = "profile"
     const header = getHeader();
     page.appendChild(header);
-
+   
+    //menu bar
     const leftSide = document.createElement("div");
-    leftSide.id = "left-side"
+    leftSide.id = "left-side";
     page.appendChild(leftSide);
+
     const buttonArea = document.createElement("div");
     buttonArea.id = "button-area";
     const back = document.createElement("button");
@@ -21,33 +23,62 @@ const createProfile = () => {
     leftSide.appendChild(buttonArea);
     buttonArea.appendChild(back);
     back.appendChild(backIcon);
+    
+    const menuArea = document.createElement("div");
+    menuArea.id = "menu-area";
+    leftSide.appendChild(menuArea)
 
+    const editButton = document.createElement("div");
+    editButton.className = "link";
+    const editText = document.createElement("h3");
+    editText.textContent = "Editar Perfil";
+    editButton.appendChild(editText);
+    menuArea.appendChild(editButton);
 
+    const helpButton = document.createElement("div");
+    helpButton.className = "link";
+    const helpText = document.createElement("h3");
+    helpText.textContent = "Central de Ajuda";
+    helpButton.appendChild(helpText);
+    menuArea.appendChild(helpButton);
 
+    //content side
     const rightSide = document.createElement("div");
     rightSide.id = "right-side";
     page.appendChild(rightSide);
 
+    //content - edit profile
+
+    const editProfile = document.createElement("div");
+    editProfile.className = "content"
+
     const background = document.createElement("div");
     background.id = "background";
-    rightSide.appendChild(background)
+    rightSide.appendChild(editProfile)
+    editProfile.appendChild(background)
 
     //profile image area
     const headerProfile = document.createElement("div");
     headerProfile.id = "header-profile";
-    rightSide.appendChild(headerProfile);
-    const profileImage = document.createElement("div");
+    editProfile.appendChild(headerProfile);
+    const containerImage = document.createElement("div");
+    const profileImage = document.createElement("div")
     profileImage.id = "profile-image";
-    headerProfile.appendChild(profileImage);
+    headerProfile.appendChild(containerImage);
+    containerImage.appendChild(profileImage)
 
     //username and image edit anchor
     const textarea = document.createElement("div");
     textarea.id = "name-area";
     const name = document.createElement("h2");
     name.textContent = "Name Complete";
+    const inputImg = document.createElement("input");
+    inputImg.type = "file";
+    inputImg.id = "image-input";
+    inputImg.accept = "image/*";
     const anchor = document.createElement("a");
     anchor.id = "anchor"
-    anchor.href = ""
+    anchor.href = "#"
     anchor.textContent = "Editar foto de perfil"
     const aIcon = document.createElement("img");
     aIcon.className = "profile-icon"
@@ -55,13 +86,14 @@ const createProfile = () => {
     aIcon.alt = "Icone de um lápis;"
     textarea.appendChild(name);
     textarea.appendChild(anchor);
+    textarea.appendChild(inputImg)
     anchor.appendChild(aIcon)
     headerProfile.appendChild(textarea)
 
     //form area
     const form = document.createElement("form");
     form.id = "form";
-    rightSide.appendChild(form)
+    editProfile.appendChild(form)
 
     //user full name input
     const nameForm = document.createElement("div");
@@ -134,7 +166,7 @@ const createProfile = () => {
     //endpage
     const endpage = document.createElement("div");
     endpage.id = "endpage";
-    rightSide.appendChild(endpage)
+    editProfile.appendChild(endpage)
 
     const save = document.createElement("button");
     save.id = "save"
@@ -145,6 +177,46 @@ const createProfile = () => {
     saveIcon.alt = "Icone de disquete"
     save.appendChild(saveIcon);
     endpage.appendChild(save)
+
+    //content - help
+    const helpTab = document.createElement("div");
+    helpTab.className = "content"
+
+
+    //logic of the page
+    helpButton.addEventListener("click", () => {
+        if(rightSide.contains(editProfile)){
+            rightSide.removeChild(editProfile);
+            rightSide.appendChild(helpTab)
+        }
+    })
+    editButton.addEventListener("click", () => {
+        if(rightSide.contains(helpTab)){
+            rightSide.removeChild(helpTab)
+            rightSide.appendChild(editProfile)
+        }
+    })
+
+    //input image logic
+    anchor.addEventListener("click", (event) => {
+        event.preventDefault();
+        inputImg.click()
+    })
+    
+    inputImg.addEventListener("change", (event) => {
+        event.preventDefault()
+        const picture = event.target.files[0];
+  
+        if(picture){
+            const reader = new FileReader();
+            reader.onload = (e) => {
+                const newPicture = e.target.result;
+                profileImage.style.backgroundImage = `url("${newPicture}")`;
+            }
+            reader.readAsDataURL(picture);
+        }
+    })
+
     return page;
 }
 export default createProfile
