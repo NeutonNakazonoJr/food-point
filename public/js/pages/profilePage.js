ae0afd1cb0d07e58ea73b92ded2d06163854f2c7
Lígia Santiago <santiago.ligia.751@gmail.com>
2024-04-18 21:31:32 -0300

    basic css in profilePage and fix pointer in guest page css while button is disabled

--- public/js/pages/profilePage.js
+++ public/js/pages/profilePage.js
@@ -5,29 +5,28 @@ const createProfile = () => {
     page.id = "profile"
     const header = getHeader();
     page.appendChild(header);
-    const body = document.createElement("div");
-    body.id = "profile-body"
-    page.appendChild(body)
 
     const leftSide = document.createElement("div");
     leftSide.id = "left-side"
-    body.appendChild(leftSide);
+    page.appendChild(leftSide);
     const buttonArea = document.createElement("div");
     buttonArea.id = "button-area";
     const back = document.createElement("button");
     back.id = "back";
     back.textContent = "Voltar";
     const backIcon = document.createElement("img");
-    backIcon.className = "profile-icon"
+    backIcon.id = "back-icon"
     backIcon.src = "./assets/icons/flag-back.svg";
     backIcon.alt = "icone de flecha para voltar"
     leftSide.appendChild(buttonArea);
     buttonArea.appendChild(back);
     back.appendChild(backIcon);
 
+
+
     const rightSide = document.createElement("div");
     rightSide.id = "right-side";
-    body.appendChild(rightSide);
+    page.appendChild(rightSide);
 
     const background = document.createElement("div");
     background.id = "background";
@@ -58,6 +57,94 @@ const createProfile = () => {
     textarea.appendChild(anchor);
     anchor.appendChild(aIcon)
     headerProfile.appendChild(textarea)
+
+    //form area
+    const form = document.createElement("form");
+    form.id = "form";
+    rightSide.appendChild(form)
+
+    //user full name input
+    const nameForm = document.createElement("div");
+    nameForm.className = "div-form"
+    const personIcon = document.createElement("img");
+    personIcon.className = "form-icon"
+    personIcon.src = "./assets/icons/userLogin.svg";
+    personIcon.alt = "Icone de pessoa";
+    const nameLabel = document.createElement("label");
+    nameLabel.className = "label"
+    nameLabel.textContent = "Nome Completo: "
+    const nameInput = document.createElement("input");
+    nameInput.className = "profile-input"
+    form.appendChild(nameForm)
+    nameForm.appendChild(personIcon)
+    nameForm.appendChild(nameLabel)
+    nameForm.appendChild(nameInput)
+
+    //user email input
+    const emailForm = document.createElement("div");
+    emailForm.className = "div-form"
+    const emailIcon = document.createElement("img");
+    emailIcon.className = "form-icon";
+    emailIcon.src = "./assets/icons/email-symbol.svg";
+    emailIcon.alt = "Icone de carta";
+    const emailLabel = document.createElement("label");
+    emailLabel.className = "label";
+    emailLabel.textContent = "Email: "
+    const emailInput = document.createElement("input");
+    emailInput.className = "profile-input";
+    form.appendChild(emailForm)
+    emailForm.appendChild(emailIcon)
+    emailForm.appendChild(emailLabel)
+    emailForm.appendChild(emailInput)
+    
+    //user password input
+    const passwordForm = document.createElement("div");
+    passwordForm.className = "div-form"
+    const passwordIcon = document.createElement("img");
+    passwordIcon.className = "form-icon";
+    passwordIcon.src = "./assets/icons/password-symbol.svg";
+    passwordIcon.alt = "Icone de cadeado";
+    const passwordLabel = document.createElement("label");
+    passwordLabel.className = "label";
+    passwordLabel.textContent = "Senha: "
+    const passwordInput = document.createElement("input");
+    passwordInput.className = "profile-input";
+    form.appendChild(passwordForm)
+    passwordForm.appendChild(passwordIcon)
+    passwordForm.appendChild(passwordLabel)
+    passwordForm.appendChild(passwordInput)
+
+    //confirm password input
+    const confirmForm = document.createElement("div");
+    confirmForm.className = "div-form"
+    const confirmIcon = document.createElement("img");
+    confirmIcon.className = "form-icon";
+    confirmIcon.src = "./assets/icons/password-symbol.svg";
+    confirmIcon.alt = "Icone de cadeado";
+    const confirmLabel = document.createElement("label");
+    confirmLabel.className = "label";
+    confirmLabel.textContent = "Confirmar Senha: "
+    const confirmInput = document.createElement("input");
+    confirmInput.className = "profile-input";
+    form.appendChild(confirmForm)
+    confirmForm.appendChild(confirmIcon)
+    confirmForm.appendChild(confirmLabel)
+    confirmForm.appendChild(confirmInput)
+
+    //endpage
+    const endpage = document.createElement("div");
+    endpage.id = "endpage";
+    rightSide.appendChild(endpage)
+
+    const save = document.createElement("button");
+    save.id = "save"
+    save.textContent = "Salvar"
+    const saveIcon = document.createElement("img");
+    saveIcon.id = "save-icon"
+    saveIcon.src = "./assets/icons/save.svg"
+    saveIcon.alt = "Icone de disquete"
+    save.appendChild(saveIcon);
+    endpage.appendChild(save)
     return page;
 }
 export default createProfile
